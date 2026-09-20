7196930dc9e4cf2412f56bf4d472cc1242866056
Dmytro Palii <paliydmytro2011@gmail.com>
2026-09-20 13:19:28 +0200

    Tag 16,17 enviroments

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
+import os
+from dotenv import load_dotenv
+load_dotenv()
+secret=os.getenv("SECRET_KEY")
+if not secret:
+  print("Warnung! Kein Secret Key in der .env-Datei gefunden")
+
 from database import(
     create_user_profile,
     login_user,
